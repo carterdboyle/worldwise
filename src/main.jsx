68414f8969455f8cb321f3d5de517d2ae14b19a6
carterdboyle <94463154+carterdboyle@users.noreply.github.com>
2025-03-12 12:27:02 -0300

    Loading cities feature
    Built a mock cities API using a JSON server so the the list of cities can be loaded and added a simulated delay with a spinner

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+//enabling country flags to work on Chromium based browsers for windows
+import { polyfillCountryFlagEmojis } from "country-flag-emoji-polyfill";
+polyfillCountryFlagEmojis();
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
